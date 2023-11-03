3b8fe06f1a2035bf32d5f963955813bbd9619c1a
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2023-11-03 17:18:53 +0530

    rebase

--- src/components/FilterTask.jsx
+++ src/components/FilterTask.jsx
@@ -16,31 +16,26 @@ const FilterTask = () => {
 
 
     return (
-<<<<<<< HEAD
         <div>
-            <button style={{ width: '7vw',backgroundColor: "rgb(186, 181, 181)" }} onClick={toggleList}>Filter</button>
-=======
-<<<<<<< Updated upstream
-        <>
-            <button onClick={toggleList}>Filter</button>
-=======
-        <div>
-            <button style={{ width: '7vw',backgroundColor: "rgb(186, 181, 181)" }} onClick={toggleList}>Filter</button>
->>>>>>> Stashed changes
->>>>>>> baa51dec350fab02e007c06d2f8a2e339329618d
-            {
-                isOpen && (
-                    <div className="drop-down">
-                        <div>
-                            <button onClick={() => { setFilter('ALL') }}>All</button>
-                            <button onClick={() => { setFilter('Completed') }}>Completed</button>
-                            <button onClick={() => { setFilter('Incomplete') }}>Incomplete</button>
+            <button style={{ width: '7vw', backgroundColor: "rgb(186, 181, 181)" }} onClick={toggleList}>Filter</button>
+
+            <>
+                <button onClick={toggleList}>Filter</button>
+                {
+                    isOpen && (
+                        <div className="drop-down">
+                            <div>
+                                <button onClick={() => { setFilter('ALL') }}>All</button>
+                                <button onClick={() => { setFilter('Completed') }}>Completed</button>
+                                <button onClick={() => { setFilter('Incomplete') }}>Incomplete</button>
+                            </div>
                         </div>
-                    </div>
-                )
-            }
+                    )
+                }
+            </>
         </div>
     )
+
 }
 
 export default FilterTask
